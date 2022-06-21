75a4e6ec5b227ee348e58f8da8d3c08959b91581
Chernikov Nikita <14nik20@gmail.com>
2022-06-22 01:43:42 +0300

    Added method ChangePhoneNumber to the CustomerManager

--- Business/Managers/CustomerManager.cs
+++ Business/Managers/CustomerManager.cs
@@ -9,6 +9,13 @@ namespace Business.Managers
 
         private static int _id = 1;
 
+        public Func<string, bool>? IsValidPhoneNumber { get; set; } = s =>
+        {
+            if (string.IsNullOrEmpty(s))
+                return false;
+            return s.First() == '+' && s.Length >= 5 && s[1..].All(c => char.IsDigit(c));
+        };
+
         public CustomerManager(RentalContext context)
         {
             Context = context ?? throw new ArgumentNullException(nameof(context));
@@ -20,11 +27,22 @@ namespace Business.Managers
                 throw new ArgumentNullException(nameof(customer));
             if (Context.Customers.Any(c => c.PhoneNumber == customer.PhoneNumber))
                 throw new InvalidOperationException("User with such a phone number already exists");
+            if (IsValidPhoneNumber != null && !IsValidPhoneNumber.Invoke(customer.PhoneNumber))
+                throw new ArgumentException("The phone number is not valid", nameof(customer));
             customer.Id = _id;
             var manager = new CustomerPasswordManager(customer);
             manager.ResetPassword(password);
             Context.Customers.Add(customer);
             _id++;
+        }
+
+        public void ChangePhoneNumber(Customer customer, string phoneNumber)
+        {
+            if (Context.Customers.Any(c => c.PhoneNumber == customer.PhoneNumber))
+                throw new InvalidOperationException("User with such a phone number already exists");
+            if (IsValidPhoneNumber != null && !IsValidPhoneNumber.Invoke(customer.PhoneNumber))
+                throw new ArgumentException("The phone number is not valid", nameof(customer));
+            customer.PhoneNumber = phoneNumber;
         }
 
         public void DeleteById(int id)
